134fed71402b6542e03bb58e2a2e4916d460fab2
LogaashreeV <cardanjude865@gmail.com>
2026-06-08 08:25:12 +0800

    chnaging ui for hotel

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import dns from "node:dns/promises";
-dns.setServers(["8.8.8.8","1.1.1.1"]);
+const dns = require("dns");
+
+dns.setServers([
+  "8.8.8.8",
+  "1.1.1.1"
+]);
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
